1030c24cc4289c6aa28c465074ef8fd28264fd47
Jose Ahumada <jahumada@webcreek.com>
2019-04-27 18:00:28 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 //Rutas
 import { APP_ROUTES } from './app.routes';
 
-import { AppRoutingModule } from './app-routing.module';
+//Modulos
+import { PagesModule } from './pages/pages.module';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent   
+    // DashboardComponent,
+    // ProgressComponent,
+    // Grafica1Component,
+    // PagesComponent,
+    // NopagefoundComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent,
+
   ],
   imports: [
     BrowserModule,
     APP_ROUTES,
-    AppRoutingModule
+    PagesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
+
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
 
 
 
 const appRoutes: Routes = [
-    { 
-        path: '',
-        component: PagesComponent,
-        children: [
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Grafica1Component },
-            { path: 'dashboard', component: DashboardComponent },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ]
-    },
-    
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
 ];
 
-export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash:true});
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true});
