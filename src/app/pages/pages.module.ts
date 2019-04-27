21470c5bfeb54a889d242c5d5ccd7718b84df34f
Jose Ahumada <jahumada@webcreek.com>
2019-04-27 18:04:42 -0500

    agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+    declarations: [ 
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Grafica1Component
+        ],
+    exports:[
+        DashboardComponent,
+        ProgressComponent,
+        Grafica1Component
+    ],
+    imports:[
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule{}
